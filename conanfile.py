bfb7984d0b658c5abeb91221a783cb71c9ed7561
Jerry W <jerrywiltse@gmail.com>
2017-08-01 10:22:37 -0400

    updated requires

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,12 @@ class BoostAlignConan(ConanFile):
     description = "For a description of this library, please visit http://boost.org/align "
     license = "www.boost.org/users/license.html"
     lib_short_name = "align"
-
+    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.64.0@bincrafters/testing"
+    
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
