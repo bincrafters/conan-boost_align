9b30f8b32c1204feb078a27266aebb8f6e78feb2
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:30 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,14 +3,14 @@ from conans import ConanFile
 
 class BoostAlignConan(ConanFile):
     name = "Boost.Align"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing"
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing"
 
     lib_short_names = ["align"]
     is_header_only = True
@@ -18,9 +18,9 @@ class BoostAlignConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-align"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
