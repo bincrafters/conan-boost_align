c048b962453f0f66be0a1cc65162bfb690c45a75
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:11:58 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostAlignConan(ConanFile):
     name = "boost_align"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_align"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["align"]
     is_header_only = True
 
